f0e1b105eb215943ca6d5513073f28148a010608
Paulina <paulina.winkowska@gmail.com>
2014-05-06 18:51:03 +0200

    FC

--- src/main/java/pl/agh/edu/jtp/autokad/ui/StateBar.java
+++ src/main/java/pl/agh/edu/jtp/autokad/ui/StateBar.java
@@ -18,34 +18,48 @@ public class StateBar extends JPanel {
     private JPanel chosenColor;
     private JLabel savedState;
 
+    private boolean allInitialized = false;
+
     public StateBar(Class figure, Color color) {
         changeFigure(figure);
         changeSaved(false);
         changeColor(color);
         this.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        allInitialized=true;
+        addIcons();
+    }
+
+    public void changeFigure(Class figure) {
+        this.removeAll();
+        JPanel figurePanel = new JPanel();
+        figurePanel.setOpaque(true);
+        getImage(figurePanel, figure);
+        this.chosenFigure = figurePanel;
+        if (allInitialized) {
+            addIcons();
+        }
+    }
+
+    private void addIcons() {
         this.add(chosenFigure);
         this.add(chosenColor);
         this.add(savedState);
     }
 
-    public void changeFigure(Class figure) {
-        JPanel figurePanel = new JPanel();
-        figurePanel.setOpaque(true);
-        getImage(figurePanel, figure);
-        this.chosenFigure = figurePanel;
-        this.removeAll();
-    }
-
     public void changeColor(Color color) {
+        this.removeAll();
         JPanel colorPanel = new JPanel();
         colorPanel.setPreferredSize(new Dimension(30, 30));
         colorPanel.setBackground(color);
         colorPanel.setOpaque(true);
         this.chosenColor = colorPanel;
-        this.repaint();
+        if (allInitialized) {
+            addIcons();
+        }
     }
 
     public void changeSaved(boolean saved) {
+        this.removeAll();
         JLabel savedLabel = new JLabel();
         if (saved) {
             savedLabel.setText("Saved");
@@ -53,6 +67,9 @@ public class StateBar extends JPanel {
             savedLabel.setText("Changed");
         }
         this.savedState = savedLabel;
+        if (allInitialized) {
+            addIcons();
+        }
     }
 
     private void getImage(JPanel panel, Class figure) {
